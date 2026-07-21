2e3c5d76594d77aa117a0a933d7f8e9024625284
Leander Chris Dsouza <122976090+leanderdsouza-0013@users.noreply.github.com>
2026-07-21 12:37:53 +0530

    Update certifications.js

--- frontend/js/certifications.js
+++ frontend/js/certifications.js
@@ -8,11 +8,11 @@ app.controller('CertificationsController', function($scope) {
     },
     {
       title: "Business Analysis & Communication Skills",
-      description: "Information Presentation, Survey Creation and Analysis with Survey Monkey, Business Analysis & Process Management"
+      description: "Presenting Information, Use SurveyMonkey to Create a Survey and Analyze Results, Business Analysis & Process Management"
     },
     {
       title: "Excel for Project & Financial Management",
-      description: "Project Management Tracker, Budget Creation, Financial Statement Creation"
+      description: "Create a Project Management Tracker using Microsoft Excel, Creating a Budget with Microsoft Excel, Create a Financial Statement using Microsoft Excel"
     },
     {
       title: "Artificial Intelligence",
