d59d7eea52726151affd4beb7dd31515b4860bb1
Leander Chris Dsouza <122976090+leanderdsouza-0013@users.noreply.github.com>
2026-07-21 12:35:09 +0530

    Update skills.js

--- frontend/js/skills.js
+++ frontend/js/skills.js
@@ -2,10 +2,10 @@ var app = angular.module('portfolioApp', []);
 
 app.controller('SkillsController', function($scope) {
   $scope.skills = [
-    ["Productivity Tools: Microsoft Excel"],
-    ["Programming & Data Handling Tools: Python, MySQL, Java"],
-    ["Full Stack Web Development: HTML, CSS, JavaScript, Bootstrap, PHP, Chart.js, AngularJs, Web Hosting"],
-    ["Analytical Skills: Exploratory Data Analysis (EDA), Data Pre-processing, Data Visualization"],
-	["Survey & Research Skills: Market Research, Survey Design"]
+    ["Programming: Python"],
+    ["Databases: SQL, MySQL"],
+    ["Data Analysis: Microsoft Excel, Exploratory Data Analysis (EDA), Data Cleaning, Data Pre-processing, Pandas, NumPy"],
+    ["Data Visualization: Tableau, Matplotlib"],
+	["Analytical & Research Skills: Market Research, Survey Design"]
   ];
 });
